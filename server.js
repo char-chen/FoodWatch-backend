08248dd5448ba8b7de20a694c5ba38b227ae5441
Matthew Zhao <matthew.x.zhao@berkeley.edu>
2016-11-13 00:25:25 -0800

    Working shit

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ server.get("/", function(req,res) {
     res.send("Yo FOOOD WATCH!", 200);
 });
 
-var frequency = {};
 //call this for every single tag
 server.get('/api/v1/service', function (req, res) {
     
@@ -18,23 +17,35 @@ server.get('/api/v1/service', function (req, res) {
     if (!Array.isArray(food)) {
         food = [food];
     }
-    
-    mongo.find("food", {"tag" : { $in : food } }, {"food" : 1}, function (err, food) {
+
+    var frequency = {};
+    mongo.find("food", {"tag" : { $in : food } }, {"_id" : 0}, function (err, foods) {
     
         if (err) {
             res.send(500, "ERROR: getActivityV1 : getActivities : DB ERROR");
             console.log("ERROR: getActivityV1 : getActivities : err : ", err.message);
-        } else if (food) {
+        } else if (foods) {
             // returns list of food associated with tag
-
-            for (var i = 0; i < food_list.length; i++) {
-                if (!(food_list[i] in frequency)) {
-                    frequency[food_list[i]] = 1;
-                } else {
-                    frequency[food_list[i]] = frequency[food_list[i]] + 1;
+            for (var i = 0; i < foods.length; i++) {
+                //console.log(foods[i].food);
+                for (var j = 0; j < foods[i].food.length; j++) {
+                    if (!(foods[i].food[j] in frequency)) {
+                        frequency[foods[i].food[j]] = 1;
+                    } else {
+                        frequency[foods[i].food[j]] = frequency[foods[i].food[j]] + 1;
+                    }
                 }
             }
-            res.send(200, food);
+            console.log(frequency);
+            var found = "";
+            var max = 0;
+            for (var food_item in frequency) {
+                if (frequency[food_item] > max) {
+                    found = food_item;
+                    max = frequency[food_item];
+                }
+            }
+            res.send(200, found);
         } else {
             res.send(500, "ERROR: getActivityV1 : getActivities  : No Activities");
             console.log("ERROR: getActivityV1 : getActivities  : No Activities");
@@ -47,16 +58,6 @@ mongo.init(function() {
     console.log("INFO: MongoDB is ready");
     server.listen(8000);
 });
-
-
-var found = "";
-var max = 0;
-for (var food in frequency) {
-    if (frequency[food] > max) {
-        found = food;
-        max = frequency[food];
-    }
-}
 
 // Do some server shit to return found :D
 
